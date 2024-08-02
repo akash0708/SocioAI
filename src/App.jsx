e1cfe21ff3b0a297bbfe964faac6547299e0137b
pi-piikachu <ahnikpurkait8846@gmail.com>
2024-08-03 04:36:08 +0530

    navbar completed and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import { BrowserRouter } from "react-router-dom";
+
 
 function App() {
   return <div className="App">
     {/* import components */}
+    <BrowserRouter>
+     <Navbar/>
+     </BrowserRouter>
     </div>;
 }
 
