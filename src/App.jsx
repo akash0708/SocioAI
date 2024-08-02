25285d8b969a30da5fd2704826d692ef087f238f
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2024-08-03 00:59:54 +0530

    hero added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import "./App.css";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+
 
 function App() {
   return <div className="App">
     {/* import components */}
+    <Hero></Hero>
       <About />
       <Contact/>
       <Footer />
---
+++ src/components/Hero.jsx
@@ -0,0 +1,51 @@
+import IeeeLogo from "../../public/ieee hero logo.png";
+import vector1 from "../../public/bg-hero-vector1.png";
+import vector2 from "../../public/bg-hero-vector2.png";
+import hands from "../../public/hand-pic-hero.png";
+
+function Hero() {
+  return (
+    <div className="h-screen relative py-28 bg-gradient-to-r from-white via-green-400 to-white overflow-hidden">
+        <div className="ellipse bg-black/20  top-0 left-[7%]"></div>
+        <div className="ellipse bg-black/20 top-0 right-[7%]"></div>
+        <div className="ellipse bg-black/15 -top-10 right-[35%]"></div>
+        <div className="ellipse bg-black/25 -bottom-64 right-[35%]"></div>
+      <img
+        className="object-contain absolute -left-24 top-20"
+        src={vector1}
+        alt=""
+      />
+      <img
+        className="object-contain absolute right-10 bottom-24"
+        src={vector2}
+        alt=""
+      />
+      <img
+        className="object-contain absolute right-0 top-1/2 transform -translate-y-1/2 w-full"
+        src={hands}
+        alt=""
+      />
+
+      <div className="flex h-full flex-col items-center justify-center gap-y-4">
+        <h1 className="text-4xl font-bold text-[#010101] text-center leading-[140%]">
+          IEEE COMPUTER SOCIETY <br />
+          CHAPTER KOLKATA SECTION
+        </h1>
+        <p className="text-2xl font-light text-[#0F5132]">PRESENTS</p>
+        <h1 className="text-[#1D2E28] font-bold text-[100px] leading-[100%]">
+          Socio <span className="text-black">AI</span>
+        </h1>
+        <p className="text-[#0F5132] text-2xl">
+          Showcase your <span className="text-black">Research.</span> Shape{" "}
+          <span className="text-black">Tomorrow.</span>
+        </p>
+        <div className="flex items-center gap-x-4">
+          <p className="text-[24px]">In association with</p>
+          <img src={IeeeLogo} alt="IEEE Logo" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
