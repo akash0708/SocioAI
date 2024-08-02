d4281771c0b7ada2da1e5a38c89207b8f19b48a4
Abhishek-465 <abhishekbhattacharjee12345@gmail.com>
2024-08-02 23:57:36 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return <div className="App">
     {/* import components */}
       <About />
+      <Contact/>
       <Footer />
     </div>;
 }
