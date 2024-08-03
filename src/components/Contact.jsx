68b0cbcdd24d1554c372ff57773932f349e44d4d
Abhishek Bhattacharjee <127030695+Abhishek-465@users.noreply.github.com>
2024-08-03 14:01:46 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,21 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   FaPhoneAlt,
   FaEnvelope,
   FaMapMarkerAlt,
   FaTwitter,
   FaInstagram,
-} from "react-icons/fa";
-import { db } from "../firebaseConfig";
-import { collection, addDoc } from "firebase/firestore";
+} from 'react-icons/fa';
+import { db } from '../firebaseConfig';
+import { collection, addDoc } from 'firebase/firestore';
 
 const Contact = () => {
   const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    subject: "",
-    message: "",
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    message: '',
   });
 
   const handleChange = (e) => {
@@ -30,42 +29,41 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await addDoc(collection(db, "contacts"), formData);
-      alert("Message sent successfully!");
+      await addDoc(collection(db, 'contacts'), formData);
+      alert('Message sent successfully!');
       setFormData({
-        firstName: "",
-        lastName: "",
-        email: "",
-        phone: "",
-        subject: "",
-        message: "",
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        message: '',
       });
     } catch (error) {
-      console.error("Error adding document: ", error);
+      console.error('Error adding document: ', error);
     }
   };
 
   return (
     <section
       id="contact"
-      className="min-h-screen w-full flex items-center text-[#0F5132] justify-center p-4 md:pt-2 font-poppins"
+      className="lg:min-h-screen h-fit w-full flex items-center text-[#0F5132] justify-center p-7 lg:p-2  font-poppins"
     >
-      <div className="container xl:max-w-6xl lg:max-w-5xl flex flex-col lg:flex-row justify-center lg:items-center w-full">
-        <div className="flex flex-col justify-between mb-8 md:mb-0 w-full lg:w-[50%] lg:mb-[85px] xl:mr-9">
-          <h1 className="text-[#1D2E28] text-4xl md:text-6xl lg:text-6xl font-bold mb-6 xl:mb-8 text-center lg:text-left">
+      <div className="container xl:max-w-6xl lg:max-w-5xl flex flex-col md:flex-row justify-center lg:items-center w-full">
+        <div className="flex flex-col justify-between mb-2 md:mb-0 w-full lg:w-[50%]  md:mr-9">
+          <h1 className="text-[#1D2E28] text-5xl lg:text-6xl xl:text-[64px] font-bold mb-6 xl:mb-8 text-center md:text-left">
             CONTACT US
           </h1>
-          <div className="space-y-7 w-full max-w-sm mx-auto lg:mx-0 lg:text-[16px]">
-            <div className="flex items-center justify-center lg:justify-start">
-              <FaPhoneAlt className="mr-2" />
+          <div className="space-y-7 w-full max-w-[340px] mx-auto md:mx-0 lg:text-[16px]">
+            <div className="flex items-center justify-center md:justify-start mr-[10px] md:mr-0 ">
+              <FaPhoneAlt className="mr-4" />
               <a href="tel:+918910398827">+91 89103 98827</a>
             </div>
-            <div className="flex items-center justify-center lg:justify-start">
-              <FaEnvelope className="mr-3" />
+            <div className="flex items-center justify-center md:justify-start ml-[40px] md:ml-0 ">
+              <FaEnvelope className="mr-4" />
               <a href="mailto:jaduniv.ieee@gmail.com">jaduniv.ieee@gmail.com</a>
             </div>
-            <div className="flex items-start justify-center lg:justify-start">
-              <FaMapMarkerAlt className="mr-2.5" />
+            <div className="flex items-start justify-center md:justify-start ml-[92px] md:ml-0 w-[180px] md:w-auto  ">
+              <FaMapMarkerAlt className="mr-4" />
               <span>
                 Jadavpur University
                 <br />
@@ -75,12 +73,12 @@ const Contact = () => {
               </span>
             </div>
           </div>
-          <div className="flex space-x-6 mt-12 mb-4 justify-center lg:justify-start">
-            <FaTwitter className="hover:text-gray-500 text-2xl" />
-            <FaInstagram className="hover:text-gray-500 text-2xl" />
+          <div className="flex space-x-6 mt-12 mb-4  justify-center md:justify-start ">
+            <FaTwitter className="hover:text-gray-500 text-[22px]" />
+            <FaInstagram className="hover:text-gray-500 text-[22px]" />
           </div>
         </div>
-        <div className="w-full lg:w-[55%] bg-[#c2c7bf] py-5 px-7 mb-12 rounded-xl">
+        <div className="w-full lg:w-[55%] bg-[#c2c7bf] py-5 px-7 lg:mt-[25px] rounded-xl">
           <form onSubmit={handleSubmit}>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-2">
               <input
