7a37d120952aea1b0edd54238276802a0dc81dd6
rahul-p19 <rahulpandey.dps@gmail.com>
2024-08-03 15:55:16 +0530

    added judge cards

---
+++ src/assets/judges.js
@@ -0,0 +1,20 @@
+const judges = [
+    {
+        "number" : 1,
+        "name" : "Judge 1 Name",
+        "designation" :"Designation",
+        "picture" : "https://conferenceoeh.com/wp-content/uploads/profile-pic-dummy.png",
+        "instagram" : "",
+        "linkedin" : ""
+    },
+    {
+        "number" : 2,
+        "name" : " Judge 2 Name",
+        "designation" :"Designation",
+        "picture" : "https://conferenceoeh.com/wp-content/uploads/profile-pic-dummy.png",
+        "instagram" : "",
+        "linkedin" : ""
+    }
+];
+
+export default judges;
