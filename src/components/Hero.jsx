7c635b86a1a7431882010dce0d9f334b5f344898
Kushaj Mallick <mystinkybutt01@gmail.com>
2024-08-08 20:49:39 +0530

    reverted to original hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,13 @@ import IeeeLogo from "../../public/ieee hero logo.png";
 import vector1 from "../../public/bg-hero-vector1.png";
 import vector2 from "../../public/bg-hero-vector2.png";
 import hands from "../../public/hand-pic-hero.png";
+import logo from "../../public/ieee logo.png";
+import kolkata from "../assets/kolkata.png";
+import QRsocioai from "../../public/QRsocioai.png";
 
 function Hero() {
   return (
-    <div className="h-screen relative py-28 bg-gradient-to-r from-white via-green-400 to-white overflow-hidden">
+    <div className="h-screen relative py-28  bg-gradient-to-r from-white via-green-400 to-white  ">
       <div className="ellipse bg-black/20  top-0 left-[7%]"></div>
       <div className="ellipse bg-black/20 top-0 right-[7%]"></div>
       <div className="ellipse bg-black/15 -top-10 right-[35%]"></div>
@@ -21,36 +24,74 @@ function Hero() {
         alt=""
       />
       <img
-        className="object-contain absolute right-0 top-1/2 transform -translate-y-1/2 w-full"
+        className="visible object-contain absolute right-0 top-1/2 transform -translate-y-1/2 w-full"
         src={hands}
         alt=""
       />
-
-      <div className="flex h-full flex-col items-center justify-center gap-y-4">
-        <h1 className="text-4xl font-bold text-[#010101] text-center leading-[140%]">
+       
+      <div className="flex h-full flex-col items-center justify-center gap-y-4 lg:mt-[6.5rem] ">
+        {/* <h1
+          className="lg:text-4xl text-xl md:text-3xl font-bold text-[#010101] text-center leading-[140%]"
+          style={{ fontFamily: "JetBrains Mono" }}
+        >
           IEEE COMPUTER SOCIETY <br />
           CHAPTER KOLKATA SECTION
+        </h1> */}
+        <div className="w-80 h-36">
+          <img src={kolkata} alt="" className="w-full h-full object-cover" />
+        </div>
+        <p
+          className="lg:text-2xl  text-lg font-light text-[#0F5132] tracking-widest"
+          style={{ fontFamily: "JetBrains Mono" }}
+        >
+          PRESENTS
+        </p>
+        <h1
+          className="text-[#1D2E28] font-bold lg:text-[150px] md:text-6xl text-5xl lg:leading-[187.5px]"
+          style={{ fontFamily: "Gothic A1" }}
+        >
+          Socio<span className="text-black">AI</span>
         </h1>
-        <p className="text-2xl font-light text-[#0F5132]">PRESENTS</p>
-        <h1 className="text-[#1D2E28] font-bold text-[100px] leading-[100%]">
-          Socio <span className="text-black">AI</span>
-        </h1>
-        <p className="text-[#0F5132] text-2xl">
+        <p
+          className="text-[#0F5132] lg:text-2xl md:text-lg text-sm text-center"
+          style={{ fontFamily: "JetBrains Mono" }}
+        >
           Showcase your <span className="text-black">Research.</span> Shape{" "}
           <span className="text-black">Tomorrow.</span>
         </p>
-        <div className="flex items-center gap-x-4">
-          <p className="text-[24px]">In association with</p>
-          <img src={IeeeLogo} alt="IEEE Logo" />
+        <div
+          className="flex items-center gap-4 md:flex-row flex-col"
+          style={{ fontFamily: "JetBrains Mono" }}
+        >
+          <p className="lg:text-[24px] text-md font-semibold">
+            In association with
+          </p>
+          <div className="inline w-48 h-20">
+            <img
+              src={IeeeLogo}
+              alt="IEEE Logo"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          {/* <img className="lg:h-16 h-10" src={logo} alt="IEEE Logo" /> */}
         </div>
         <div>
           <a
-            href="https://forms.gle/uXxHnAHNdnkLJaBDA"
+            href="https://bit.ly/socioai"
             target="_blank"
-            className="bg-[#1D2E28] text-white font-bold text-2xl px-6 py-3 rounded-lg relative top-4"
+            className="bg-[#1D2E28] text-white font-bold lg:text-2xl text-md px-6 py-3 rounded-lg relative top-4"
+            style={{ fontFamily: "JetBrains Mono" }}
           >
             Register Now
           </a>
+          
+        </div>
+        <div className="inline w-[10rem] h-[10rem] mt-8 ">
+        <img
+              src={QRsocioai}
+              alt="Register QR"
+              className="w-full h-full object-cover"
+            />
         </div>
       </div>
     </div>
